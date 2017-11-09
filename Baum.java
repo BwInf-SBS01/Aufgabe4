1e3336aea4b5f5412d151763237a262a08b43da1
SBSJavatar <31701319+SBSJavatar@users.noreply.github.com>
2017-11-09 17:15:26 +0100

    Add files via upload

--- Baum.java
+++ Baum.java
@@ -1,49 +1,61 @@
-import java.util.ArrayList;
-
+@SuppressWarnings("unused")
 public class Baum {
 
-  private String string;
-  private boolean gefunden = false;
-  private KnotenA rootKnoten;
-  private KnotenB gefundenerKnoten;
+	private String string; // Speichert den übergebenen String, der Dargetellt werden
+							// soll.
+	private boolean gefunden = false; // Markierung, ob schon eine Möglichkeit der
+										// darstllung gefunden wurde.
+	private KnotenA rootKnoten; // Speichert den ersten/root Knoten.
+	private KnotenB gefundenerKnoten; // Speichert den letzten B-Konten der für das
+										// Schreiben wichtig ist.
 
-  public Baum(String string) {
-    this.string = string;
+	public Baum(String string) {
+		this.string = string;
+		rootKnoten = new KnotenA(0, string.length() - 1, this, null); // Ersellt denersten Knoten (stelle = 0, länge
+																		// =stringlänge,die Speicheradresse des Baums,
+																		// null -> keinübergeordneterKnoten)
+	}
 
-    rootKnoten = new KnotenA(0, string.length() - 1, this, null);
-  }
+	public String getSubstring(int stelle1, int stelle2) { // Funktion für die Knoten;
+															// gibt einen Teil, des
+		return this.string.substring(stelle1, stelle2);// darzustellenden Wortes, damit
+														// der Knoten ihn dann
+														// überprüfen kann.
+	}
 
-  public String getString() {
-    return string;
-  }
+	public boolean isGefunden() { // Funktion für die Knoten; gibt Auskunft, ob weiter
+									// gesucht werden soll.
+		return this.gefunden;
+	}
 
-  @Override
-  public String toString() {
+	public void setGefunden(KnotenB knoten) {// Setzen der Markierung; wird vom letzten
+												// Konten aufgerufen, der sich
+												// selbst übergibt und dann gespeichtert
+												// wird.
+		this.gefunden = true;
+		this.gefundenerKnoten = knoten;
+	}
 
-    return "[" + string + "]";
-  }
+	public void print() {// schreibt die Kennzeichen, indem eine Liste erstellt und von
+							// dem unersten
+							// Konten immer zum nächst höheren gegeben wird und dabei
+							// ergänzt wird
+		if (gefunden) {
+			java.util.ArrayList<String> list = new java.util.ArrayList<String>();
+			this.gefundenerKnoten.getParent(list);
+			list.trimToSize();
+			for (int i = 0; i < list.size(); i += 2) {
+				System.out.print(list.get(list.size() - i - 1) + "-" + list.get(list.size() - i - 2) + " ");
+			}
+		} else {
+			System.out.print("nicht Darstellbar");
+		}
+		System.out.println("");
+	}
 
-  public boolean isGefunden() {
-    return gefunden;
-  }
-
-  public void setGefunden(KnotenB knoten) {
-    this.gefunden = true;
-    gefundenerKnoten = knoten;
-  }
-
-  public void print() {
-    if (gefunden) {
-      ArrayList<String> list = new ArrayList<String>();
-      gefundenerKnoten.getParent(list);
-      list.trimToSize();
-      for (int i = 0; i < list.size(); i += 2) {
-        System.out.print(list.get(list.size() - i - 1) + "-" + list.get(list.size() - i - 2) + " ");
-      }
-    } else {
-      System.out.print("nicht Darstellbar");
-    }
-    System.out.println("");
-  }
+	@Override
+	public String toString() {
+		return "[" + string + "]";
+	}
 
 }
--- KnotenB.java
+++ KnotenB.java
@@ -1,34 +1,34 @@
 public class KnotenB {
-  private KnotenA parent;
-  private String segment;
+	private KnotenA parent;
+	private String segment;
 
-  public KnotenB(int stelle, int length, Baum baum, KnotenA parent) {
-    this.parent = parent;
+	public KnotenB(int stelle, int length, Baum baum, KnotenA parent) {
+		this.parent = parent;
 
-    if (stelle == length && !baum.isGefunden()) {
-      segment = baum.getString().substring(stelle, stelle + 1);
-      baum.setGefunden(this);
-      return;
-    }
-    if (stelle == length && !baum.isGefunden()) {
-      segment = baum.getString().substring(stelle, stelle + 2);
-      baum.setGefunden(this);
-      return;
-    }
-    if (stelle + 1 < length && !baum.isGefunden()) {
-      segment = baum.getString().substring(stelle, stelle + 1);
-      new KnotenA(stelle + 1, length, baum, this);
-    }
-    if (stelle + 2 < length && !baum.isGefunden()) {
-      segment = baum.getString().substring(stelle, stelle + 2);
-      new KnotenA(stelle + 2, length, baum, this);
-    }
-  }
+		if (stelle + 0 == length && !baum.isGefunden()) { //
+			segment = baum.getSubstring(stelle, stelle + 1);
+			baum.setGefunden(this);
+			return;
+		}
+		if (stelle + 1 == length && !baum.isGefunden()) {
+			segment = baum.getSubstring(stelle, stelle + 2);
+			baum.setGefunden(this);
+			return;
+		}
+		if (stelle + 1 < length && !baum.isGefunden()) {
+			segment = baum.getSubstring(stelle, stelle + 1);
+			new KnotenA(stelle + 1, length, baum, this);
+		}
+		if (stelle + 2 < length && !baum.isGefunden()) {
+			segment = baum.getSubstring(stelle, stelle + 2);
+			new KnotenA(stelle + 2, length, baum, this);
+		}
+	}
 
-  public void getParent(java.util.ArrayList<String> list) {
-    list.add(segment);
-    if (parent != null) {
-      parent.getParent(list);
-    }
-  }
+	public void getParent(java.util.ArrayList<String> list) {
+		list.add(segment);
+		if (parent != null) {
+			parent.getParent(list);
+		}
+	}
 }
